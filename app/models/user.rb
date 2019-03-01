4207e6509b6192722255d7859dc345c1f4d728f5
Anna Lornitzo <lornitzoa@gmail.com>
2019-03-01 17:16:09 -0500

    all user routes functional

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,13 +33,14 @@ class User
   def self.create(opts)
     results = DB.exec(
       <<-SQL
-        INSTER INTO users
+        INSERT INTO users
           (username, password, birthday, image, bio)
         VALUES
           ('#{opts['name']}', '#{opts['password']}', DATE '#{opts['birthday']}', '#{opts['image']}', '#{opts['bio']}')
         RETURNING id, username, password, birthday, image, bio
       SQL
     )
+    result = results.first
     return {
       'id' => result['id'].to_i,
       'username' => result['username'],
@@ -50,6 +51,41 @@ class User
     }
   end
 
+  def self.delete(id)
+    restuls = DB.exec(
+      <<-SQL
+        DELETE FROM users WHERE id = #{id}
+      SQL
+    )
+    return {
+      'deleted': true
+    }
+  end
+
+  def self.update(id, opts)
+    results = DB.exec(
+      <<-SQL
+        UPDATE users
+        SET
+          username='#{opts['username']}',
+          password='#{opts['password']}',
+          birthday='#{opts['birthday']}',
+          image='#{opts['image']}',
+          bio='#{opts['bio']}'
+        WHERE id = #{id}
+        RETURNING id, username, password, birthday, image, bio;
+      SQL
+    )
+    result = results.first
+    return {
+      'id' => result['id'].to_i,
+      'username' => result['username'],
+      'password' => result['password'],
+      'birthday' => result['birthday'],
+      'image' => result['image'],
+      'bio' => result['bio']
+    }
+  end
 
 
 end
