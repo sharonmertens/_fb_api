4099b2367ae5c235c4c21f43a9955212950628e4
Anna Lornitzo <lornitzoa@gmail.com>
2019-03-01 18:00:22 -0500

    modification to user.rb to deploy heroku attempt

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User
     uri = URI.parse(ENV['DATABASE_URL'])
     DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
   else
-    DB = PG.connect({:host => 'localhost', :port => 5432, :dbname => '_fb_api_development'})
+    DB = PG.connect(:host => 'localhost', :port => 5432, :dbname => '_fb_api_development')
   end
   def self.all
     results = DB.exec(
