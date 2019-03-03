82fa650901e71ed88cebd81eaf8ff824e84e5570
Anna Lornitzo <lornitzoa@gmail.com>
2019-03-03 16:31:57 -0500

    decode token complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::API
+
+  def authenticate_token
+   # puts "AUTHENTICATE JWT"
+   render json: { status: 401, message: 'Unauthorized' } unless decode_token(bearer_token)
+ end
+
+ def bearer_token
+   # puts "BEARER TOKEN"
+   header = request.env["HTTP_AUTHORIZATION"]
+
+   pattern = /^Bearer /
+   # puts 'TOKEN WITHOUT BEARER'
+   header.gsub(pattern, '') if header && header.match(pattern)
+ end
+
+ def decode_token(token_input)
+   JWT.decode(token_input, ENV['JWT_SECRET'], true)
+ rescue
+   render json: {status: 401, message: 'Unauthorized'}
+ end
+
 end
