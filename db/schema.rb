d6b4a15d03d50b681ac7b7a4c7a2a619ff8b0a3e
Anna Lornitzo <lornitzoa@gmail.com>
2019-03-04 21:33:05 -0500

    backend authorization complete

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,24 @@ ActiveRecord::Schema.define(version: 2019_03_02_015010) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "posts", id: false, force: :cascade do |t|
+    t.serial "id", null: false
+    t.text "text"
+    t.string "image", limit: 256
+    t.string "link", limit: 256
+    t.integer "likes"
+    t.integer "dislikes"
+    t.string "author", limit: 256
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "birthday"
+    t.string "image", limit: 256
+    t.text "bio"
   end
 
 end
