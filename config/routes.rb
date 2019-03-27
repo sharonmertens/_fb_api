dede9a8bec1a515f313f49d2935bd13c574555bc
Anna Lornitzo <lornitzoa@gmail.com>
2019-03-27 14:38:38 -0400

    making sure schema is right for heroku db

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Heroku Welcome Page
+
+
 
   # ------------------------
   #      AUTHENTICATION      #
